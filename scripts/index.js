e6f2527b770bcbabd630d0e254e04083fce196c6
arty gvozdenkov <gvozdenkov@gmail.com>
2022-05-25 05:08:00 +0300

    refactor: adopt form validation for all inputs all forms (but not working now:)

--- scripts/index.js
+++ scripts/index.js
@@ -6,30 +6,40 @@ function getErrorSpan(inputElem) {
   return inputElem.nextElementSibling;
 }
 
-function showInputError(element, errorMassage) {
-  element.classList.add("form-04__input_type_error");
-  let errorSpan = getErrorSpan(element);
+function showInputError(formElement, inputElement, errorMassage) {
+  inputElement.classList.add("form-04__input_type_error");
+  let errorSpan = getErrorSpan(inputElement);
   errorSpan.textContent = errorMassage;
   errorSpan.classList.add("form-04__input-error_active");
 }
 
-function hideInputError(element) {
-  element.classList.remove("form-04__input_type_error");
-  const errorSpan = getErrorSpan(element);
+function hideInputError(formElement, inputElement) {
+  inputElement.classList.remove("form-04__input_type_error");
+  const errorSpan = getErrorSpan(inputElement);
   errorSpan.textContent = "";
   errorSpan.classList.remove("form-04__input-error_active");
 }
 
-function isValid(evt) {
-  const inputElem = evt.target;
-  inputElem.validity.valid
-    ? hideInputError(inputElem)
-    : showInputError(inputElem, inputElem.validationMessage);
+function isValid(formElement, inputElement) {
+  inputElement.validity.valid
+    ? hideInputError(formElement, inputElement)
+    : showInputError(formElement, inputElement, inputElement.validationMessage);
 }
 
-formInputs.forEach((input) => input.addEventListener("input", isValid));
+function setEventListeners(formElement) {
+  inputList = formElement.querySelectorAll(".form-04__input");
+  inputList.forEach((inputElement) => {
+    console.log("hello");
+    inputElement.forEach((input) => input.addEventListener("input", isValid));
+  });
+}
 
-form.addEventListener("submit", (evt) => {
-  console.log(`hello from ${evt.target.name}`);
-  evt.preventDefault();
-});
+function enableValidation() {
+  const formList = document.querySelectorAll(".form-04");
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => evt.preventDefault());
+    setEventListeners(formElement);
+  });
+}
+
+enableValidation();
